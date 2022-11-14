99965fd41d3d620dc47468c1f1fbb6832f75f7ab
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-14 20:55:58 +0600

    abstract user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,31 @@
-from django.db import models # noqa
+"""Contains database models"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager class for custom user model"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """create a new user and return an instance of the user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
