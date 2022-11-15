480b6c77ce9f15ce9767a11107508bc5568061aa
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-15 19:06:03 +0600

    user and superuser create test cases created

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,32 @@
+"""Test for admin modifications"""
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """test for admin """
+
+    def setUp(self):
+        """test on creating user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects. \
+            create_superuser(  # type: ignore
+            email='admin.s@example.com',
+            password='passtest12345'
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(  # type: ignore
+            email='user@example.com',
+            password="testpass123",
+            name="New User"
+        )
+
+    def test_user_list(self):
+        """Test if user are listed or not"""
+        url = reverse('admin:core_user_change_list')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
