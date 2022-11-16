b0b9a01183a150250688594e5fe2cdab04205b65
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-17 01:27:24 +0600

    recipe model and test for model

--- app/core/models.py
+++ app/core/models.py
@@ -1,5 +1,6 @@
 """Contains database models"""
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -40,3 +41,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe model"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Contains all the test cases for models of core app
 """
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
+
+from core import models
 
 
 class ModelTests(TestCase):
@@ -12,7 +16,7 @@ class ModelTests(TestCase):
         """run a test to create a user with email"""
         email = 'test@example.com'
         password = 'testpass123'
-        user = get_user_model().objects.create_user(
+        user = get_user_model().objects.create_user(  # type:ignore
             email=email,
             password=password
         )
@@ -30,20 +34,37 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in sample_emails:
-            user = get_user_model().objects.create_user(email, 'sample123')
+            user = get_user_model().objects.create_user(  # type:ignore
+                email, 'sample123'
+                )
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raise_error(self):
         """Tast that if a user is creating without a email
             raises a ValueError"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'test123')
+            get_user_model().objects.create_user('', 'test123')  # type:ignore
 
     def test_create_superuser(self):
         """Test creating a super user"""
-        user = get_user_model().objects.create_superuser(
+        user = get_user_model().objects.create_superuser(  # type:ignore
             'test123@example.com',
             'test123'
         )
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_creating_recipe(self):
+        """Test creating recipe model is success"""
+        user = get_user_model().objects.create_user(  # type:ignore
+            'test@example.com',
+            'pass123test'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample Recipe Name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample Recipe Description'
+        )
+        self.assertEqual(str(recipe), recipe.title)
