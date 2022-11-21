6a1c1368de3402cf479e065332c816815d1e740c
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-21 23:34:45 +0600

    ingredient serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -3,7 +3,16 @@ Serializers for recipe APIs
 """
 from rest_framework import serializers
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for Ingredient Model Class"""
+
+    class Meta:
+        model = Ingredient
+        fields = ['id', 'name']
+        read_only_fields = ['id']
 
 
 class TagSerializer(serializers.ModelSerializer):
